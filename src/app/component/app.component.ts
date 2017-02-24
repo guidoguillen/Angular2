43c7b98d677928aedffd872b7872d7faf6298d9e
Guido Guillen <guido.guillen@ucn.net>
2017-02-24 16:53:31 -0400

    Message Demo

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -21,7 +21,7 @@ export class CampaingSummaryBySkill {
     <h1>{{title}}</h1>
     <h2>Campaing Summary By Skill</h2>
     <div>
-    <button (click)="loadReport()">Get Business Intelligence Report 2</button>
+    <button (click)="loadReport()">Demo Git Button</button>
     <table>
         <thead>
             <th align="left">Skill Id</th>
